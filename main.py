f85636007dbea349551f071ee65d789310d5f1a1
June-24 <mj21csb0b36@student.nitw.ac.in>
2023-03-15 19:46:35 +0530

    first

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import pyqrcode
 from pyqrcode import QRCode 
   
 # String which represent the QR code 
-s = "https://github.com/"
+s = "https://github.com/June-24"
   
 # Generate QR code 
 url = pyqrcode.create(s) 
   
 # Create and save the png file naming "myqr.png" 
-url.svg("myyoutube.svg", scale = 8)
+url.svg("MyGithub.png", scale = 100)
